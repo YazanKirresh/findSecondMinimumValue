6f96f216640fd261c928452a81c19abcfc3be60d
Yazan <yazan.kirresh@hotmail.com>
2024-09-10 19:26:40 +0300

    Resolved

---
+++ 905. Sort Array By Parity/main.js
@@ -0,0 +1,19 @@
+var sortArrayByParity = function(nums) {
+    var left = 0;
+    var right = 0;
+    while (left < nums.length){
+        if (nums[left] % 2 == 0) {
+            let temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            right++;
+        }
+        left++;
+    }
+    console.log(nums);
+    return nums;
+};
+
+
+let nums = [3,1,2,4];
+sortArrayByParity(nums);
